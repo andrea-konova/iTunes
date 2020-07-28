e93dd2580fa33d3aac517c7be856b1f8af2b859e
Andrii Konovalenko <an256.ak@gmail.com>
2020-07-28 15:54:08 +0300

    feat: created project iTunes

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+import { radioPlayerInit } from './radioPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
+
+const playerBtn = document.querySelectorAll('.player-btn'),
+	playerBlock = document.querySelectorAll('.player-block'),
+	temp = document.querySelector('.temp');
+
+document.addEventListener('click', () => {
+	const target = event.target;
+
+	if (target.matches('.player-btn')) {
+		playerBtn.forEach((item, i) => {
+			if (item === target) {
+				item.classList.add('active');
+				playerBlock[i].classList.add('active');
+			} else {
+				temp.style.display = 'none';
+				item.classList.remove('active');
+				playerBlock[i].classList.remove('active');
+			}
+		});
+	}
+});
+
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
