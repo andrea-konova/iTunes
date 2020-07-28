3e1c0c387f51534b5498c4ea726acff8ea6d944f
Andrii Konovalenko <an256.ak@gmail.com>
2020-07-28 17:11:34 +0300

    feat: feated videoPlayer

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,73 @@
 export const videoPlayerInit = () => {
-	console.log('videoPlayerInit');
+	const	videoPlayer = document.querySelector('.video-player'),
+		videoButtonPlay = document.querySelector('.video-button__play'),
+		videoTimePassed = document.querySelector('.video-time__passed'),
+		videoProgress = document.querySelector('.video-progress'),
+		videoTimeTotal = document.querySelector('.video-time__total');
+
+	const toggleIcon = () => {
+		if (videoPlayer.paused) {
+			videoButtonPlay.classList.remove('fa-pause');
+			videoButtonPlay.classList.add('fa-play');
+		} else {
+			videoButtonPlay.classList.add('fa-pause');
+			videoButtonPlay.classList.remove('fa-play');
+		}
+	};
+
+	const togglePlay = () => {
+		if (videoPlayer.paused) {
+			videoPlayer.play();
+		} else {
+			videoPlayer.pause();
+		}
+	};
+
+	const stopPlay = () => {
+		videoPlayer.pause();
+		videoPlayer.currentTime = 0;
+	};
+
+	const addZero = n => (n < 10 ? '0' + n : n);
+
+	const toggleTime = () => {
+		const currentTime = videoPlayer.currentTime,
+			duration = videoPlayer.duration;
+
+		videoProgress.value = (currentTime / duration) * 100;
+
+		const minutePassed = Math.floor(currentTime / 60),
+			secondsPassed = Math.floor(currentTime % 60);
+
+		const minuteTottal = Math.floor(duration / 60),
+			secondsTottal = Math.floor(duration % 60);
+
+		videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+		videoTimeTotal.textContent = `${addZero(minuteTottal)}:${addZero(secondsTottal)}`;
+	};
+
+	const toggleProgress = () => {
+		const duration = videoPlayer.duration,
+			value = videoProgress.value;
+
+		videoPlayer.currentTime = (value * duration) / 100;
+	};
+
+	document.addEventListener('click', () => {
+		const target = event.target;
+
+		if (target.matches('.video-button__play') || target.matches('.video-player')) {
+			togglePlay();
+		}
+
+		if (target.matches('.video-button__stop')) {
+			stopPlay();
+		}
+	});
+
+	videoProgress.addEventListener('change', toggleProgress);
+
+	videoPlayer.addEventListener('play', toggleIcon);
+	videoPlayer.addEventListener('pause', toggleIcon);
+	videoPlayer.addEventListener('timeupdate', toggleTime);
 };
